b0f0803c3ced4142fbbd431c151247f1db9d13dc
omar <omar.tourabi7@gmail.com>
2023-07-19 18:09:37 +0200

    feat: Text input form

--- src/components/TextInputForm.jsx
+++ src/components/TextInputForm.jsx
@@ -1,13 +1,27 @@
 /* eslint-disable react/prop-types */
-import { Box, InputLabel } from '@mui/material'
+import { Box, InputLabel, Input, FormHelperText } from '@mui/material'
+import { inputValidation } from '../utils/inputValues'
+import { useState } from 'react'
 
-export const TextInputForm = ({ label, children }) => {
+export const TextInputForm = ({ field, handler }) => {
+    const { label, error, type, validation } = inputValidation[field]
+    const [displayErr, setDisplayErr] = useState(false)
     return (
         <Box my={2}>
             <InputLabel>
                 {label}
             </InputLabel>
-            {children}
+            <Input onChange={() => setDisplayErr(false)} type={type}
+                onBlur={(e) => {
+                    if (!validation(e.target.value)) {
+                        setDisplayErr(true)
+                        e.target.value = ''
+                    }
+                    handler(field, e.target.value)
+                }} />
+            {displayErr &&
+                <FormHelperText error>{error}</FormHelperText>
+            }
         </Box>
     )
 }
