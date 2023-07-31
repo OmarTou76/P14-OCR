d1752d2ea34f2134a56408a0e42e64bc96a15949
omar <omar.tourabi7@gmail.com>
2023-07-31 11:38:19 +0200

    fix: persist data constructor

--- src/utils/Persist.js
+++ src/utils/Persist.js
@@ -1,11 +1,15 @@
 export class Persist {
     static addEmployee(employee) {
-        const employees = JSON.parse(localStorage.getItem('employees')) ?? []
-        employees.push(employee)
+        const data = JSON.parse(localStorage.getItem('employees')) ?? []
+        data.push(employee)
+        const employees = data.map((element, id) => ({
+            ...element,
+            id,
+        }))
         localStorage.setItem('employees', JSON.stringify(employees))
     }
 
     static getEmployees() {
-        return JSON.parse(localStorage.getItem('employees'))
+        return JSON.parse(localStorage.getItem('employees')) ?? []
     }
 }
